a591a4abcee9e0d50acf35ea861638306a959725
Thakurpraval <thakurpraval2023@gmail.com>
2025-11-11 19:59:07 +0000

    operatipns

--- 02/cnvt.js
+++ 02/cnvt.js
@@ -1,5 +1,12 @@
-let score ="3abc"
-console.log(typeof score);
-let isLoggedIn = 1
-let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+//let score ="3abc"
+//console.log(typeof score);
+//let isLoggedIn = 1
+//let booleanIsloggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsloggedIn);
+//@@@@@@@@@@@@@@@@@operations#####################
+let value = 3
+let negValue = -value
+//console.log(negValue);
+//cosole.log("2"+2);
+//console.log(2+2+"2");
+//console.log("1"+2+2);
