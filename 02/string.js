d307bd9273623bbf231a642b15e63c995db324d7
Thakurpraval <thakurpraval2023@gmail.com>
2025-11-15 06:17:48 +0000

    string all

---
+++ 02/string.js
@@ -0,0 +1,7 @@
+const url = "https://praval.com/praval%20thakur"
+console.log(url.replace('%20','-'))
+
+const newString = "  praval  "
+console.log(newString);
+console.log(newString.trim());
+console.log(url.includes('praval'))
