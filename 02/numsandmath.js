e1088eb8c604c771f123fe97be8ea6f5ab53156f
Thakurpraval <thakurpraval2023@gmail.com>
2025-11-15 07:57:17 +0000

    maths alll

--- 02/numsandmath.js
+++ 02/numsandmath.js
@@ -1,4 +1,25 @@
 const score = 400
-console.log(score);
+//console.log(score);
 const balance = new Number(100)
-console.log(balance);
+//console.log(balance);
+
+const othernumber = 123.4
+//console.log(othernumber.toPrecision(2));
+//console.log(othernumber.toPrecision(4));
+//console.log(othernumber.toPrecision(3));
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-In'));  
+
+
+//===================maths=========================
+console.log(Math.ceil(4.2))
+
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+const min = 10
+const max = 20
+Math.random()*(max-min+1) + min
+
+
