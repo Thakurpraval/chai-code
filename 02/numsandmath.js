ec3dfd6f5f7f1d17e8e1374914d2db98d7801354
Thakurpraval <thakurpraval2023@gmail.com>
2025-11-16 05:23:06 +0000

    extara details of maths

--- 02/numsandmath.js
+++ 02/numsandmath.js
@@ -20,6 +20,7 @@ console.log(Math.random());
 console.log(Math.random()*10+1);
 const min = 10
 const max = 20
-Math.random()*(max-min+1) + min
+console.log(Math.random()*(max-min+1))
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
 
 
