5859b41e859c1cc2ca2a199a96358e169e237ed8
Thakurpraval <thakurpraval2023@gmail.com>
2025-11-17 17:47:33 +0000

    infoobject

---
+++ 03/obejct2.js
@@ -0,0 +1,9 @@
+const tinderUser = new Object()
+tinderUser.Id = "123"
+tinderUser.name = "sammy"
+tinderUser.isloggedIn = false
+//console.log(tinderUser);
+const regularUser = {
+    email : "some123@gmail.com";
+    fullname  :
+}
